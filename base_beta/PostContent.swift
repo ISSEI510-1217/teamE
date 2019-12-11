d183b3bc3044a50c032aa6cca50189425d73550b
後藤壱成 <gotoissei.g.510@gmail.com>
2019-12-12 00:14:53 +0900

    Interfering with timeline cells

---
+++ base_beta/PostContent.swift
@@ -0,0 +1,33 @@
+//
+//  PostContent.swift
+//  base_beta
+//
+//  Created by 後藤壱成 on 2019/12/11.
+//  Copyright © 2019 kaito. All rights reserved.
+//
+
+import UIKit
+
+class PostContent: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //[戻る]ボタン
+    @IBAction func backTable(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
